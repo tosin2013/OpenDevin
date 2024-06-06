a846e24fe0739d51d0470534726e27d7410993e4
Tosin Akinosho <tosin.akinosho@gmail.com>
2024-06-06 15:23:03 -0400

    Update nginx-proxy.sh

--- nginx-proxy.sh
+++ nginx-proxy.sh
@@ -55,16 +55,17 @@ server {
     ssl_certificate ${cert_file};
     ssl_certificate_key ${key_file};
 
+    auth_basic "Restricted Content";
+    auth_basic_user_file /etc/nginx/.htpasswd;
+
     location / {
-        auth_basic "Restricted Content";
-        auth_basic_user_file /etc/nginx/.htpasswd;
         proxy_pass http://localhost:3001;
-        proxy_set_header Upgrade \$http_upgrade;
-        proxy_set_header Connection upgrade;
-        proxy_set_header Host \$host;
-        proxy_set_header X-Real-IP \$remote_addr;
-        proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
-        proxy_set_header X-Forwarded-Proto \$scheme;
+        proxy_set_header Upgrade $http_upgrade;
+        proxy_set_header Connection "upgrade";
+        proxy_set_header Host $host;
+        proxy_set_header X-Real-IP $remote_addr;
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto $scheme;
         proxy_buffering off;
     }
 }
